11e4766c402ccb071eb5a05abcb9a9d6b89d1245
baharevmv <31476163+baharevmv@users.noreply.github.com>
2018-02-11 02:07:28 +0300

    Добавил начальный экран с простой анимацией. Надеюсь ближе к делу найти какие-то более осмысленные фотографии для показа в анимации.

---
+++ VK-ObjC-project-BMV/VK-ObjC-project-BMV/Category/UIImageView+BMVImageView.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+BMVImageView.h
+//  VK-ObjC-project-BMV
+//
+//  Created by max on 09.02.18.
+//  Copyright © 2018 Maksim Bakharev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView_BMVImageView : UIImageView
+
+@end
---
+++ VK-ObjC-project-BMV/VK-ObjC-project-BMV/Category/UIImageView+BMVImageView.m
@@ -0,0 +1,21 @@
+//
+//  UIImageView+BMVImageView.m
+//  VK-ObjC-project-BMV
+//
+//  Created by max on 09.02.18.
+//  Copyright © 2018 Maksim Bakharev. All rights reserved.
+//
+
+#import "UIImageView+BMVImageView.h"
+
+@implementation UIImageView_BMVImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
